561dc72587ac68e0ca6b89320117bad3d6e7df35
Marian Bajan <marianbajan1502@yahoo.com>
2024-05-25 16:58:24 +0300

    MH-2 made improvements to statistic retrieval files and generated stats for btc when time frame is 1d

--- statistics_retrievar/get_stats.py
+++ statistics_retrievar/get_stats.py
@@ -2,57 +2,83 @@ import argparse
 import sys
 from typing import Dict
 
-from data_analyser import DataAnalyser
+from data_analyser import CandleColor, DataAnalyser
 
 SUPPORTED_COMMANDS_LIST = [
-    "count_candles_by_color", 
-    "group_candles_by_percentage_change"
+    "count_candles_by_color",
+    "group_candles_by_percentage_change",
 ]
 
+
 def help() -> None:
-    print(f"""{sys.argv[0]} usage:
+    print(
+        f"""{sys.argv[0]} usage:
 1. command: python {sys.argv[0]} -cmd count_candles_by_color -i infile.csv -o outfile
-2. command: python {sys.argv[0]} -cmd group_candles_by_percentage_change -i infile.csv -bz 2 -o outfile """)
+2. command: python {sys.argv[0]} -cmd group_candles_by_percentage_change -i infile.csv -bz 2 -o outfile """
+    )
     exit(1)
 
+
 def main():
-    
+
     if len(sys.argv) == 1:
         help()
-    
-    parser = argparse.ArgumentParser(description='Get statistics based on Open and Close prices in a CSV file.')
-    parser.add_argument('-i', '--input', type=str, required=True, help='Path to the input CSV file.')
-    parser.add_argument('-o', '--output', type=str, required=True, help='Path to output file.')
-    parser.add_argument('-cmd', '--command', type=str, required=True, help=f'Action to be performed.\
-                        Supported actions are: {" ".join([f"{index + 1}.{item}" for index, item in enumerate(SUPPORTED_COMMANDS_LIST)])}')
-    parser.add_argument('-bz', '--bucket_size', type=int, required=False, help='Optional. Bucket size used when grouping candles.')
-    
+
+    parser = argparse.ArgumentParser(
+        description="Get statistics based on Open and Close prices in a CSV file."
+    )
+    parser.add_argument(
+        "-i", "--input", type=str, required=True, help="Path to the input CSV file."
+    )
+    parser.add_argument("-o", "--out", type=str, required=True, help="Path to the output file.")
+    parser.add_argument(
+        "-cmd",
+        "--command",
+        type=str,
+        required=True,
+        help=f'Action to be performed. Supported actions: {" ".join([f"{index + 1}.{item}" for index, item in enumerate(SUPPORTED_COMMANDS_LIST)])}',
+    )
+    parser.add_argument(
+        "-bz",
+        "--bucket_size",
+        type=int,
+        required=False,
+        help="Optional. Bucket size used when grouping candles.",
+    )
+
     args = parser.parse_args()
-    
+
     analyser = DataAnalyser(args.input)
     analyser.lazyload_data()
-    
-    with open(args.output, 'w+') as f:
-     
+
+    with open(args.out, "w+") as f:
+
         if args.command == "count_candles_by_color":
-            
-            green_candles_count, red_candles_count, gray_candles_count = analyser.count_candles_by_color()
-            f.write(f"Green candles: {green_candles_count}.\n")
-            f.write(f"Red candles: {red_candles_count}.\n")
-            f.write(f"No-change candles: {gray_candles_count}.\n")
-            
+            f.writelines(
+                [
+                    "This file contains the frequency of green/red/doji candles in the dataset.\n",
+                    'In the context of trading, a candle that does not change the price is often called a "doji".\n\n',
+                ]
+            )
+            colors = analyser.count_candles_by_color()
+            f.write(f"Green candles: {colors[CandleColor.GREEN]}.\n")
+            f.write(f"Red candles: {colors[CandleColor.RED]}.\n")
+            f.write(f"Doji candles: {colors[CandleColor.GREY]}.\n")
+
         elif args.command == "group_candles_by_percentage_change":
-            
-            candles_grouping: Dict[int, int] = analyser.group_candles_by_percentage_change(args.bucket_size)
+
+            candles_grouping: Dict[int, int] = analyser.group_candles_by_percentage_change(
+                args.bucket_size
+            )
             for key in sorted(candles_grouping.keys()):
                 interval_info = f"[{key}% -> {key + args.bucket_size - 0.01}%]"
-                # Zero values will be skipped when writing the results.   
+                # Zero values will be skipped when writing the results.
                 if candles_grouping.get(key) != 0:
                     f.write(f"Interval {interval_info} has {candles_grouping.get(key)} samples.\n")
-    
+
         else:
             print(f"Wrong command! Supported commands are: {SUPPORTED_COMMANDS_LIST}.")
-     
-        
+
+
 if __name__ == "__main__":
     main()
--- statistics_retrievar/similar_candles_in_a_row.py
+++ statistics_retrievar/similar_candles_in_a_row.py
@@ -1,28 +1,59 @@
-import os
+import argparse
 
-from data_analyser import DataAnalyser
+from data_analyser import CandleColor, DataAnalyser
+
+MAX_IN_A_ROW = 20  # We expect a maximum number of 20 consecutive candles of the same color
 
 
 def main():
-    analyser = DataAnalyser(f"./data/bitcoin_info.csv")
+
+    parser = argparse.ArgumentParser(
+        description="Counts groups of K similar candles in a row - regarding color."
+    )
+    parser.add_argument(
+        "-i", "--input", type=str, required=True, help="Path to the input CSV file."
+    )
+    parser.add_argument("-o", "--out", type=str, required=True, help="Output file.")
+
+    args = parser.parse_args()
+    analyser = DataAnalyser(args.input)
     analyser.lazyload_data()
 
-    my_dict = {}
-    maxi = 20
-    for i in range (2,maxi):
-        my_dict[i] = analyser.count_consecutive_candles(i, "green")
-    
-    print("GREEN dict: ", my_dict)
-    for i in range(2,maxi-1):
-        print(f"consecutive {i+1} / consecutive {i}: {my_dict[i+1]/my_dict[i]}")
-        
-    for i in range (2,maxi):
-        my_dict[i] = analyser.count_consecutive_candles(i, "red")
-    
-    print("RED dict: ", my_dict)
-    for i in range(2,maxi-1):
-        print(f"consecutive {i+1} / consecutive {i}: {my_dict[i+1]/my_dict[i]}")
-        
+    with open(args.out, "w+") as f:
+
+        f.writelines(
+            [
+                "The objective is to count consecutive candles that share the same color.\n"
+                "For instance, I want to determine HOW MANY TIMES 3 green candles appear consecutively. Let's call this result X.\n"
+                "Next, I want to find out HOW MANY TIMES 4 green candles appear consecutively. Let's call this result Y.\n"
+                "Imagine we have a chart in front of us.\n"
+                "We see 3 GREEN candles and want to know the probability that the next candle will also be GREEN.\n"
+                "This probability is given by Y/X.\n"
+                "Note that in a dataset with a sequence like red->green->green->green->green->red, we only count 1 group of 4 green candles.\n"
+            ]
+        )
+        # Stats about GREEN consecutive candles
+        groups_count = {}
+        for i in range(2, MAX_IN_A_ROW):
+            groups_count[i] = analyser.count_consecutive_candles(i, CandleColor.GREEN)
+
+        groups_count = {k: v for k, v in groups_count.items() if v != 0}  # filter out 0 values
+        f.write(f"\nGREEN dict: {groups_count}\n")
+
+        for i in range(2, len(groups_count) + 1):
+            f.write(f"consecutive {i+1} / consecutive {i}: {"{:.3f}".format(groups_count[i+1]/groups_count[i])}\n")
+
+        # Stats about RED consecutive candles
+        groups_count = {}
+        for i in range(2, MAX_IN_A_ROW):
+            groups_count[i] = analyser.count_consecutive_candles(i, CandleColor.RED)
+
+        groups_count = {k: v for k, v in groups_count.items() if v != 0}  # filter out 0 values
+        f.write(f"\nRED dict: {groups_count}\n")
+
+        for i in range(2, len(groups_count) + 1):
+            f.write(f"consecutive {i+1} / consecutive {i}: {"{:.3f}".format(groups_count[i+1]/groups_count[i])}\n")
+
 
 if __name__ == "__main__":
     main()
